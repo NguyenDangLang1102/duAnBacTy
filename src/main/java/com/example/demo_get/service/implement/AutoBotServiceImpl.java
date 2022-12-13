1a4ea58c491580281d3ac07bbc9cd3c7201f34d6
TrungLe <trungle2001yf@gmail.com>
2022-12-13 10:45:14 +0700

    "tinhtien"

--- src/main/java/com/example/demo_get/service/implement/AutoBotServiceImpl.java
+++ src/main/java/com/example/demo_get/service/implement/AutoBotServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-
 import javax.transaction.Transactional;
 import java.util.Random;
 
@@ -22,19 +21,32 @@ import java.util.Random;
 
 public class AutoBotServiceImpl implements AutobotService {
     private static int allBotPrice = 0;
+    private static int timeSleep = 0;
+    private static int maxStockVolume = 0;
+    private static int minStockVolume = 0;
 
-   @Autowired
+
+
+    @Autowired
     private UserRepository userRepository;
-
 
     @Override
     public UserRespond insert(AutoBotIn autoBotIn) {
         allBotPrice= autoBotIn.getAllBotPrice();
-        return null;
+        timeSleep = autoBotIn.getTimeSleep();
+        if (autoBotIn.getMinStockVolume() <= autoBotIn.getMaxStockVolume()){
+            maxStockVolume = autoBotIn.getMaxStockVolume();
+            minStockVolume = autoBotIn.getMinStockVolume();
+            return null;
+        }else {
+            return new UserRespond("Min ?");
+        }
     }
 
     @Override
     @Scheduled(cron = "0 0/15 9-13/45 * * *")
+    @Scheduled(fixedDelayString = "PT2s")
+
     //tao ra cac command chay deu 15/lan tu 9h-18h
     public UserRespond commandAutoBot() {// start va stop thu cong
         if (allBotPrice == 0){
@@ -42,17 +54,20 @@ public class AutoBotServiceImpl implements AutobotService {
         }else {
             Random random = new Random();
             String[] userName = {"Trung", "Lang", "Minh", "Huy"};
+            String[] codeStock  = {"cow", "pig", "horse", "rooster", "hen","dog","cat","donkey"};
             Boolean isSale = random.nextBoolean();
-            int stockPrice = random.nextInt(11) + 20;
-            int stockNumber = random.nextInt(51);
+            int stockPrice = random.nextInt(11) + 20;// random.nextInt(max + 1 - min) + min
+            int stockNumber = random.nextInt(maxStockVolume + 1 - minStockVolume) + minStockVolume;
             int realBotPrice = allBotPrice - stockNumber * stockPrice;//so tien con lai
-            allBotPrice = realBotPrice;
-            System.out.println("gggggg   " + allBotPrice);
             if (allBotPrice > (stockNumber * stockPrice)) {
-                userRepository.insertCommand(userName[random.nextInt(4)], "cow", isSale, stockPrice, stockNumber);
+                userRepository.insertCommand(userName[random.nextInt(2)], codeStock[random.nextInt(8)], isSale, stockPrice, stockNumber);
+                try {
+                    Thread.sleep(timeSleep);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 return null;
             } else {
-
                 return null;
             }
         }
